308bb28ccb20497b32e6b8d48af1523974e6062e
Brian Kuperman <kupe517@gmail.com>
2018-04-17 14:16:38 -0700

    Cleanup
    Added layout options
    Cleaned up the code to be less repetitive

--- Set Column Size.sketchplugin/Contents/Sketch/plugin.js
+++ Set Column Size.sketchplugin/Contents/Sketch/plugin.js
@@ -81,65 +81,11 @@ function setArtboardWidth(){
 |--------------------------------------------------------------------------
 */
 
-var columnsSet1 = function(context) {
+var resizeColumns = function(context){
+	var newSize = context.command.identifier().replace('columnsSet', '');
 	setVariables(context);
-	setColumns(1);
-};
-
-var columnsSet2 = function(context) {
-	setVariables(context);
-	setColumns(2);
-};
-
-var columnsSet3 = function(context) {
-	setVariables(context);
-	setColumns(3);
-};
-
-var columnsSet4 = function(context) {
-	setVariables(context);
-	setColumns(4);
-};
-
-var columnsSet5 = function(context) {
-	setVariables(context);
-	setColumns(5);
-};
-
-var columnsSet6 = function(context) {
-	setVariables(context);
-	setColumns(6);
-};
-
-var columnsSet7 = function(context) {
-	setVariables(context);
-	setColumns(7);
-};
-
-var columnsSet8 = function(context) {
-	setVariables(context);
-	setColumns(8);
-};
-
-var columnsSet9 = function(context) {
-	setVariables(context);
-	setColumns(9);
-};
-
-var columnsSet10 = function(context) {
-	setVariables(context);
-	setColumns(10);
-};
-
-var columnsSet11 = function(context) {
-	setVariables(context);
-	setColumns(11);
-};
-
-var columnsSet12 = function(context) {
-	setVariables(context);
-	setColumns(12);
-};
+	setColumns(newSize);
+}
 
 var columnsSetArtboardWidth = function(context) {
 	setVariables(context);
@@ -153,71 +99,11 @@ var columnsSetArtboardWidth = function(context) {
 |--------------------------------------------------------------------------
 */
 
-var columnsAlign0 = function(context){
+var setAlignment = function(context){
+	var alignment = context.command.identifier().replace('columnsAlign', '');
 	setVariables(context);
-	alignColumns(0);
-};
-
-var columnsAlign1 = function(context){
-	setVariables(context);
-	alignColumns(1);
-};
-
-var columnsAlign2 = function(context){
-	setVariables(context);
-	alignColumns(2);
-};
-
-var columnsAlign3 = function(context){
-	setVariables(context);
-	alignColumns(3);
-};
-
-var columnsAlign4 = function(context){
-	setVariables(context);
-	alignColumns(4);
-};
-
-var columnsAlign5 = function(context){
-	setVariables(context);
-	alignColumns(5);
-};
-
-var columnsAlign6 = function(context){
-	setVariables(context);
-	alignColumns(6);
-};
-
-var columnsAlign7 = function(context){
-	setVariables(context);
-	alignColumns(7);
-};
-
-var columnsAlign8 = function(context){
-	setVariables(context);
-	alignColumns(8);
-};
-
-var columnsAlign9 = function(context){
-	setVariables(context);
-	alignColumns(9);
-};
-
-var columnsAlign10 = function(context){
-	setVariables(context);
-	alignColumns(10);
-};
-
-var columnsAlign11 = function(context){
-	setVariables(context);
-	alignColumns(11);
-};
-
-var columnsAlign12 = function(context){
-	setVariables(context);
-	alignColumns(12);
-};
-
+	alignColumns(alignment);
+}
 
 function alignColumns(val){
 	var col1_x = Number((artboardWidth - gridWidth) / 2);
@@ -291,4 +177,27 @@ function updateParentFrames(layer){
     parent.adjustToFit();
     parent = parent.parent;
   }
+}
+
+/* Layout Settings */
+
+function centerLayout(context){
+	setVariables(context);
+	var layout = MSLayoutGrid.alloc().init();
+	layout.setHorizontalOffset(layoutOffset);
+	layout.setIsEnabled(true);
+	artboard.setLayout(layout);
+}
+
+function setLayout(context){
+	var layoutSize = context.command.identifier().replace('layout', '');
+	setVariables(context);
+	var layout = MSLayoutGrid.alloc().init();
+	layout.setTotalWidth(layoutSize);
+	layout.setNumberOfColumns(12);
+	layout.setGutterWidth(20);
+	var layoutOffset = (artboardWidth - layoutSize) / 2;
+	layout.setHorizontalOffset(layoutOffset);
+	layout.setIsEnabled(true);
+	artboard.setLayout(layout);
 }
